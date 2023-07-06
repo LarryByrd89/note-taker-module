44ba7ea9a56279ac384d7e9f74713cf15133c388
LarryByrd89 <laryn.n99@gmail.com>
2023-07-05 20:46:43 -0400

    conencted proper links

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
 //HTML
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
@@ -14,10 +18,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
   });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
-
+//API
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db.json', 'utf8', (err, data) => {
     if (err) {
@@ -40,3 +41,54 @@ app.post('/api/notes', (req, res) => {
     }
 
     const notes = JSON.parse(data);
+
+    // Assign a unique id to the new note
+    newNote.id = Date.now();
+
+    notes.push(newNote);
+
+    fs.writeFile('./db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Error writing note.' });
+      }
+
+      res.json(newNote);
+    });
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id);
+
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'Error reading notes.' });
+    }
+
+    const notes = JSON.parse(data);
+
+    const index = notes.findIndex((note) => note.id === noteId);
+
+    if (index === -1) {
+      return res.status(404).json({ error: 'Note not found.' });
+    }
+
+    const deletedNote = notes.splice(index, 1)[0];
+
+    fs.writeFile('./db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Error writing note.' });
+      }
+
+      res.json(deletedNote);
+    });
+  });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});

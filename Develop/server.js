faead4d0e1a9b7f5aab0bff713b3747f93485608
LarryByrd89 <laryn.n99@gmail.com>
2023-07-05 20:28:45 -0400

    added api

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 //HTML
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
@@ -10,3 +13,30 @@ app.get('/notes', (req, res) => {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
   });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'Error reading notes.' });
+    }
+
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+
+  fs.readFile('./db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'Error reading notes.' });
+    }
+
+    const notes = JSON.parse(data);

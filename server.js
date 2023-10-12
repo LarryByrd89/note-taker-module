d4b404203901c91b8229063835e4e83acc03e463
LarryByrd89 <laryn.n99@gmail.com>
2023-10-12 15:11:51 -0400

    save note function fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,89 +10,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Serve the notes.html page
+let notes = [];
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+  res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
-// Serve the index.html page for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
-// Retrieve all notes
 app.get('/api/notes', (req, res) => {
-  fs.readFile('./db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ error: 'Error reading notes.' });
-    }
-
-    const notes = JSON.parse(data);
-    res.json(notes);
-  });
+  res.json(notes);
 });
 
-// Save a new note
+//save notes and create unique id
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
-
-  fs.readFile('./db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ error: 'Error reading notes.' });
-    }
-
-    const notes = JSON.parse(data);
-
-    // Assign a unique id to the new note
-    newNote.id = Date.now();
-
-    notes.push(newNote);
-
-    fs.writeFile('./db.json', JSON.stringify(notes), (err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ error: 'Error writing note.' });
-      }
-
-      res.json(newNote);
-    });
-  });
+  newNote.id = uuidv4();
+  notes.push(newNote);
+  res.json(newNote);
 });
 
-// Delete a note
+//Delete notes
 app.delete('/api/notes/:id', (req, res) => {
-  const noteId = parseInt(req.params.id);
-
-  fs.readFile('./db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ error: 'Error reading notes.' });
-    }
-
-    const notes = JSON.parse(data);
-
-    const index = notes.findIndex((note) => note.id === noteId);
-
-    if (index === -1) {
-      return res.status(404).json({ error: 'Note not found.' });
-    }
-
-    const deletedNote = notes.splice(index, 1)[0];
-
-    fs.writeFile('./db.json', JSON.stringify(notes), (err) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ error: 'Error writing note.' });
-      }
-
-      res.json(deletedNote);
-    });
-  });
+  const id = req.params.id;
+  notes = notes.filter((note) => note.id !== id);
+  res.json({ message: 'Note deleted' });
 });
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`App listening on PORT ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
